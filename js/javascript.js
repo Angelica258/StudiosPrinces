71c61a9f9c71aa83c06d328ee4301929f06f3247
Angelik Bermudez <32436970+Angelica258@users.noreply.github.com>
2018-09-28 20:34:16 -0500

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+$("a").click(function (event){
+ 
+    if ($(this).data('seleccion') != null){
+        console.log($(this).data('seleccion'));
+        if ($(this).data('seleccion') === "index"){
+        	
+            window.location.href = "index.html";
+        } else {
+            $("#contenedor2").load('paginas/'+$(this).data('seleccion')+'.html');
+        }
+    }
+});
+$(document).ready( function() {
+    $('#myCarousel').carousel({
+		interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#myCarousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+});
